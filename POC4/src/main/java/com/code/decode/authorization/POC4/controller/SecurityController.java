f89c167d0d21cd7391e344af3bfe7004058d257d
Mustofa Adny <adniadni04@gmail.com>
2026-05-25 17:13:31 +0700

    method level preAuthorize

--- POC4/src/main/java/com/code/decode/authorization/POC4/controller/SecurityController.java
+++ POC4/src/main/java/com/code/decode/authorization/POC4/controller/SecurityController.java
@@ -1,17 +1,30 @@
 package com.code.decode.authorization.POC4.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
     @GetMapping("/testMe")
     public String getTest() {
+        SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().forEach(a -> System.out.println(a));
         return "code test";
     }
 
     @GetMapping("/status")
     public String getStatus() {
+        SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().forEach(a -> System.out.println(a));
         return "code status";
+    }
+
+    @PreAuthorize("#id == authentication.principal.username")
+    @GetMapping("testMe/{id}")
+    public String testGetV1DemoEndpoint(@PathVariable String id) {
+        SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().forEach(a -> System.out.println(a));
+
+        return "Access granted to user: " + id;
     }
 }
